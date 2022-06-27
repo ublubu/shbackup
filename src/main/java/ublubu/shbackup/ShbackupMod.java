47e877c5f43d94267f3f4f7f8aa3dc4f54798140
Kynan Rilee <kynan.rilee@formation.ai>
2022-06-27 12:48:48 -0400

    remember to reload from gradle tab

--- src/main/java/ublubu/shbackup/ShbackupMod.java
+++ src/main/java/ublubu/shbackup/ShbackupMod.java
@@ -5,6 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ShbackupMod implements ModInitializer {
+	public static final String MOD_ID = "shbackup";
+
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
@@ -12,6 +14,8 @@ public class ShbackupMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		LOGGER.info("Starting Shbackup by ublubu");
+
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
